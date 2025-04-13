efc2d974866f8ac313440e25b2ed5a918721d9fb
Dragisa Krsmanovic <dragisa@crunchbase.com>
2025-04-12 17:15:00 -0700

    build

---
+++ init/init-db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id         SERIAL PRIMARY KEY,
+    username   VARCHAR(50)  NOT NULL UNIQUE,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE users
+    REPLICA IDENTITY full;
