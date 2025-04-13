a5c8fc4985815e467e279c667460f35a7a58a1ad
Dragisa Krsmanovic <dragisa@crunchbase.com>
2025-04-13 12:16:57 -0700

    key

--- src/main/scala/postgres/replication/protocol/Column.scala
+++ src/main/scala/postgres/replication/protocol/Column.scala
@@ -4,14 +4,25 @@ import scodec.Codec
 import scodec.codecs.*
 
 case class Column(
-    columnFlags: Int,
+    columnFlags: Column.ColumnFlag,
     columnName: String,
     oidOfColumnDataType: Int,
     typeModifier: Int
 )
 
 object Column {
-  implicit val codec: Codec[Column] = (int8 ::
+
+  sealed trait ColumnFlag
+  object ColumnFlag {
+    case object NoFlag    extends ColumnFlag
+    case object PartOfKey extends ColumnFlag
+    implicit val codec: Codec[ColumnFlag] = discriminated[ColumnFlag]
+      .by(int8)
+      .typecase(0, provide(NoFlag))
+      .typecase(1, provide(PartOfKey))
+  }
+
+  implicit val codec: Codec[Column] = (ColumnFlag.codec ::
     cstring ::
     oidCodec ::
     int32).as[Column]
